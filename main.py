ce8d86572a30b824699c59792841af069627425d
Frederique <f.c.m.damstra@student.rug.nl>
2026-06-03 12:03:28 +0200

    Update main.py
    Update based on pragmatics.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ This file runs the pipeline of the different linguistics fields used to determin
 import pandas as pd
 import spacy
 from semantics import determine_semantics_story
+from pragmatics import determine_pragmatic_story
 
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('coreferee')
@@ -15,16 +16,16 @@ DEV_CSV = "dev.csv"
 def main():
 
     '''
-    This function combines all other python files, using a voting system to determine whether text is a story or not
+    This function combines all other python files, using a voting system to determine whether text is a story or not.
     '''
 
     total = 0
     total_correct = 0
-    combined_correct = 0
 
     syntax_correct = 0
     semantics_correct = 0
     pragmatic_correct = 0
+    combined_correct = 0
 
 
     df = pd.read_csv(DEV_CSV)
@@ -38,8 +39,7 @@ def main():
         #syntax_story = determine_syntax_story(doc)
 
         semantics_story = determine_semantics_story(doc)
-
-        #pragmatic_story = determine_pragmatic_story(doc)
+        pragmatic_story = determine_pragmatic_story(text)
 
         #if syntax_story == label:
         #    syntax_correct += 1
@@ -47,9 +47,10 @@ def main():
         if semantics_story == label:
             semantics_correct += 1
             total_correct += 1
-        #if pragmatic_story == label:
-        #    pragmatic_correct += 1
-        #    total_correct += 1
+        if pragmatic_story == label:
+            pragmatic_correct += 1
+            total_correct += 1
+
     print(f'percentage correct: {round(total_correct/total, 2)}')
 
 if __name__ == "__main__":
